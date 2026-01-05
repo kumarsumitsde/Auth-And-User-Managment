cfeeb44b556a8b8cef2c59f41f174451f95651a1
Sumit Kumar <sumitkumar192113920@gmail.com>
2026-01-05 23:31:28 +0530

    gateway

---
+++ api-gateway/src/main/java/com/api_gateway/api_gateway/util/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.api_gateway.api_gateway.util;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public Claims getClaims (String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
